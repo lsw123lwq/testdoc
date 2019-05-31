27baafac51f9db13d723d37854fddbfe0c3e32cf
kanas.l <kanas.l@daumkakao.com>
2019-05-31 15:35:34 +0900

    no message

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <p>ㅁㄴ아ㅓㅏㅓㅁ너ㅏㅏㅁㄴ
+    어ㅏㅁㄴ아ㅣㅓㅁㄴ아ㅣㅓㅁㄴㅇㅁㄴ아ㅓㅁㄴㅇ
+    ㅁㄴ이ㅏ먼이ㅏ먼ㅇ
+    ㅁㄴㅇㅁㄴ</p>
+    <p>ㅁㄴ아ㅓㅏㅓㅁ너ㅏㅏㅁㄴ
+      어ㅏㅁㄴ아ㅣㅓㅁㄴ아ㅣㅓㅁㄴㅇㅁㄴ아ㅓㅁㄴㅇ
+      ㅁㄴ이ㅏ먼이ㅏ먼ㅇ
+      ㅁㄴㅇㅁㄴ</p>
+    <p>ㅁㄴ아ㅓㅏㅓㅁ너ㅏㅏㅁㄴ
+      어ㅏㅁㄴ아ㅣㅓㅁㄴ아ㅣㅓㅁㄴㅇㅁㄴ아ㅓㅁㄴㅇ
+      ㅁㄴ이ㅏ먼이ㅏ먼ㅇ
+      ㅁㄴㅇㅁㄴ</p>
+    <p>ㅁㄴ아ㅓㅏㅓㅁ너ㅏㅏㅁㄴ
+      어ㅏㅁㄴ아ㅣㅓㅁㄴ아ㅣㅓㅁㄴㅇㅁㄴ아ㅓㅁㄴㅇ
+      ㅁㄴ이ㅏ먼이ㅏ먼ㅇ
+      ㅁㄴㅇㅁㄴ</p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
